47ab04a45a4952462410b30e20f6fc33a4e31bb9
twiposhy <miha.emelya.03@gmail.com>
2026-04-23 13:25:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import cv2
 import pytesseract
 import openpyxl
-pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files (x86)/Tesseract-OCR/tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"PATH TO TESSERACT"
 
 
 # Загрузка изображения автомобиля
-image = cv2.imread("C:/Users/Acer/PycharmProjects/sam_sdelal_oop/car.jpg")
+image = cv2.imread("PATH TO CAR IMAGE")
 
 # Предобработка изображения для улучшения точности распознавания
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -19,4 +19,4 @@ text = pytesseract.image_to_string(gray, lang='eng', config='--psm 11')
 wb = openpyxl.Workbook()
 ws = wb.active
 ws['A1'] = text
-wb.save("C:/Users/Acer/PycharmProjects/sam_sdelal_oop/car_number.xlsx")
+wb.save("PATH TO XLSX FILE")
